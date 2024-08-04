8bb56b5066a1462c7e2074240f03f5efe787b911
Aaditdot-1234 <kumud2196@gmail.com>
2024-08-04 20:57:57 +0530

    some small changes.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,8 +4,14 @@ import friends from '../../public/friends.png'
 
 const Home = () => {
   return (
-    <div className={styles.home}>
-        <img src={friends} alt="not found" />
+    <div className={styles.home}> 
+      <div>
+        <p>Welcome to Mind First</p>
+        <h2>Putting 'Mind First' is not selfish; it's essential.</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+        <hr></hr>
+      </div>
+      <img src={friends} alt="not found" />
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,16 @@ const Navbar = () => {
   return (
     <div className={styles.navbar}>
         <div className={styles.header}>
-            <h1>Equillibrium</h1>
+            <h1>Mind First</h1>
         </div>  
         <div className={styles.navbarComp}> 
           <a href="home">Home</a>   
-          <a href="news">News</a>  
+          <a href="news">News</a> 
+          <a href="contacts">Contacts</a>  
           <a href="Login">Login</a> 
+        </div>
+        <div className={styles.appointment}>
+          <button>Appointment</button>
         </div> 
     </div>  )
 }
