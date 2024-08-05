c289c64017c33968dc68c5e003ff855ed3e3a5ee
Aaditdot-1234 <kumud2196@gmail.com>
2024-08-06 02:13:20 +0530

    minute changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import './App.css'
 
 function App() {
   return (
-    <body>  
+    <>
       <Navbar/> 
       <Home/>
-    </body>
+    </>
   )
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import styles from '../components/Home.module.css'
 import friends from '../../public/friends.png'
+import arrow from '../../public/arrow.png'
 
 const Home = () => {
   return (
     <div className={styles.home}> 
       <div>
-        <p>Welcome to Mind First</p>
-        <h2>Putting 'Mind First' is not selfish; it's essential.</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
-        <hr></hr>
-      </div>
-      <img src={friends} alt="not found" />
-    </div>
+        <div>
+          <p>Welcome to Mind First</p>
+          <h2>Putting 'Mind First' is not selfish; it's essential.</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p> 
+          <button>
+            TEST
+            <img src={arrow} alt="not found" />
+          </button>
+        </div>
+        <img src={friends} alt="not found" />
+      </div>   
+    </div> 
   )
 }
 
